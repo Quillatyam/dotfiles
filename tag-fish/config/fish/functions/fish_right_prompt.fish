a72cd3eacdd7f88cbddd20a3ec5c5708b559bfc8
Robert den Harink <robert@robhar.com>
2018-02-08 11:35:52 +0100

    Removed loading upstream git info in propmt, this causes slow behaviour in big repositories.
    Test plan: *
    
    Revert plan: *
    
    Reviewed by: *

--- tag-fish/config/fish/functions/fish_right_prompt.fish
+++ tag-fish/config/fish/functions/fish_right_prompt.fish
@@ -6,9 +6,9 @@ set cyan (set_color -o cyan)
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showuntrackedfiles 'no'
+set __fish_git_prompt_showupstream 'no'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
