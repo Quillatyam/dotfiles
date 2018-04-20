3ed93565854024516500a1a12ace83d316d12918
Robert den Harink <robert@robhar.com>
2018-04-20 14:02:58 +0200

    Switched terminal emulator

--- tag-fish/config/fish/functions/fish_right_prompt.fish
+++ tag-fish/config/fish/functions/fish_right_prompt.fish
@@ -7,17 +7,17 @@ set cyan (set_color -o cyan)
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'no'
-set __fish_git_prompt_showuntrackedfiles 'no'
+set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'no'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_dirtystate 'D'
+set __fish_git_prompt_char_stagedstate 'S'
+set __fish_git_prompt_char_untrackedfiles 'U'
+#set __fish_git_prompt_char_stashstate ''
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
